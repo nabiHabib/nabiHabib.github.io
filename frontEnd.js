ec2bdd2d3fc310c26527cf5dfe7dbb1e82b5697b
Nabi Habib <nabihabib96@gmail.com>
2023-09-14 00:54:48 -0700

    change link

--- frontEnd.js
+++ frontEnd.js
@@ -3,6 +3,6 @@ const pdfViewer = document.getElementById("pdfViewer");
 const pdfContainer = document.getElementById("pdfContainer");
 
 showResumeButton.addEventListener("click", function() { 
-    pdfViewer.src = "https://github.com/nabiHabib/nabiHabib.github.io/blob/main/Software%20Engineer%20Resume%20(Web).pdf";
+    pdfViewer.src = "https://github.com/nabiHabib/nabiHabib.github.io/blob/main/SoftwareEngineerResumeWeb.pdf";
     pdfContainer.style.display = "block";
 });
