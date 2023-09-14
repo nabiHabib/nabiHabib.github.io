9fff3b36e2bf7de4236f8052c68c8c364c4ed05a
Nabi Habib <nabihabib96@gmail.com>
2023-09-14 01:00:21 -0700

    changed link to embedded

--- frontEnd.js
+++ frontEnd.js
@@ -3,6 +3,6 @@ const pdfViewer = document.getElementById("pdfViewer");
 const pdfContainer = document.getElementById("pdfContainer");
 
 showResumeButton.addEventListener("click", function() { 
-    pdfViewer.src = "https://github.com/nabiHabib/nabiHabib.github.io/blob/main/SoftwareEngineerResumeWeb.pdf";
+    pdfViewer.src = "SoftwareEngineerResumeWeb.pdf";
     pdfContainer.style.display = "block";
 });
