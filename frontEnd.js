5d7265abff4257a7fe685a960af06bed832675ee
Nabi Habib <nabihabib96@gmail.com>
2023-09-14 00:43:47 -0700

    cleaned up .js

--- frontEnd.js
+++ frontEnd.js
@@ -1,11 +1,8 @@
-<script>
-    const showResumeButton = document.getElementById("showResumeButton");
-    const pdfViewer = document.getElementById("pdfViewer");
-    const pdfContainer = document.getElementById("pdfContainer");
+const showResumeButton = document.getElementById("showResumeButton");
+const pdfViewer = document.getElementById("pdfViewer");
+const pdfContainer = document.getElementById("pdfContainer");
 
-    showResumeButton.addEventListener("click", function() {
-        const pdfUrl = "https://github.com/nabiHabib/nabiHabib.github.io/blob/main/Software%20Engineer%20Resume%20(Web).pdf";        
-        pdfViewer.src = pdfUrl;
-        pdfContainer.style.display = "block";
-    });
-</script>
+showResumeButton.addEventListener("click", function() { 
+    pdfViewer.src = "https://github.com/nabiHabib/nabiHabib.github.io/blob/main/Software%20Engineer%20Resume%20(Web).pdf";
+    pdfContainer.style.display = "block";
+});
