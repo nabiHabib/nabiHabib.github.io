d2cf826fc11c917bf6fd0b28d36ed86254396501
Nabi Habib <nabihabib96@gmail.com>
2023-09-14 02:20:34 -0700

    added about me button funct

--- frontEnd.js
+++ frontEnd.js
@@ -1,6 +1,16 @@
 const showResumeButton = document.getElementById("showResumeButton");
 const pdfViewer = document.getElementById("pdfViewer");
 const pdfContainer = document.getElementById("pdfContainer");
+const showAboutMeButton = document.getElementById("showAboutMeButton");
+const aboutMeContainer = document.getElementById("aboutMeContainer");
+
+showAboutMeButton.addEventListener("click", function() {
+    if (aboutMeContainer.style.display === "none" || aboutMeContainer.style.display === "") {
+        aboutMeContainer.style.display = "block";
+    } else {
+        aboutMeContainer.style.display = "none";
+    }    
+});
 
 showResumeButton.addEventListener("click", function() { 
     pdfViewer.src = "SoftwareEngineerResumeWeb.pdf";
