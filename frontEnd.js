33d755b0bd6126fc762e10f2818ff8d4d22043a5
Nabi Habib <nabihabib96@gmail.com>
2023-09-17 18:42:27 -0700

    fixed button click object dissapear

--- frontEnd.js
+++ frontEnd.js
@@ -8,17 +8,13 @@ showAboutMeButton.addEventListener("click", function() {
     if (aboutMeContainer.style.display === "none" || aboutMeContainer.style.display === "") {
         aboutMeContainer.style.display = "block";
         pdfContainer.style.display = "none";
-    } else {
-        aboutMeContainer.style.display = "none";
-    }    
+    }   
 });
 
 showResumeButton.addEventListener("click", function() { 
     if (pdfContainer.style.display === "none" || pdfContainer.style.display === "") {
         pdfContainer.style.display = "block";
         aboutMeContainer.style.display = "none"; // Hide the about me section
-    } else {
-        pdfContainer.style.display = "none"; // Hide the resume section
-    }
+    } 
     pdfViewer.src = "SoftwareEngineerResumeWeb.pdf";
 });
